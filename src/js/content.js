1741cc98db7f1c424fd063add7b5f535a153f3ba
hungtin <hungtin.bk12@gmail.com>
2018-04-20 16:48:56 +0900

    add connect between content js and butotn handler

--- src/js/content.js
+++ src/js/content.js
@@ -1,3 +1,8 @@
-console.log("Hello from content script");
-console.log("new");
-console.log("new more");
+(function() {
+  const $ = require("jquery");
+  console.log("Content js is loaded");
+  chrome.runtime.onMessage.addListener(function(req, sender, res) {
+    var p = $("<p></p>").text(req.eth);
+    $("body").append(p);
+  });
+})();
--- src/js/view/Button.js
+++ src/js/view/Button.js
@@ -10,6 +10,21 @@ class Button extends Widget {
   }
 
   onClick(evt) {
+    console.log(evt);
+    // send the data that is pointed by this button to content script.
+    var data = {
+      email: "hungtin.bk12@gmail.com",
+      eth: "0x827b26dd4ca03dd6d5ea68db43c9688f07d24d53",
+      firstName: "Tin",
+      lastName: "Trinh"
+    };
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, data, function(res) {
+        console.log(res);
+      });
+    });
     console.log("Click on me");
   }
 }
